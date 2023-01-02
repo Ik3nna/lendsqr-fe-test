a2468d97db5d6461c42831f15c57831da53b96c7
Ik3nna <iamiykendukwu@gmail.com>
2023-01-02 14:42:44 +0100

    Feat: Setup of the login page and addition of routing to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login/login";
+
+function App () {
+    return(
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/home" element={<Login />} />
+
+                    <Route path="/" element={<Navigate replace to="/home" />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App;
 
---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./login.module.scss";
+
+function Login () {
+    return(
+        <main>
+            
+        </main>
+    )
+}
+
+export default Login;
