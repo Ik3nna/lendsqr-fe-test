841c687fa04db8ae5d0b1655831d9fbc73304cef
Ik3nna <iamiykendukwu@gmail.com>
2023-01-02 15:30:29 +0100

    Feat: Setup of the login page and addition of form validation using react-bootstrap.
    BugFix: Fixed the import of the global.scss file

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from "react-router-dom"
 import styles from "./login.module.scss";
 
 function Login () {
+    const [validated, setValidated] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            navigate("/dashboard");
+        }
+
+        setValidated(true);
+    };
+
     return(
         <main>
-            
+            <section>
+                <img src="/assets/Group.svg" alt="logo" />
+
+                <img src="/pablo-sign-in 1.svg" alt="pablo" />
+            </section>
+
+            <section>
+                <h4>Welcome!</h4>
+                <p>Enter details to login</p>
+
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group controlId="validationCustom01">
+                        <Form.Control
+                            required
+                            type="email"
+                            placeholder="Email"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please enter a valid email address
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="validationCustom01">
+                        <Form.Control
+                            required
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Group>
+
+                    <div>forgot password</div>
+
+                    <Button type="submit">log in</Button>
+                </Form>
+            </section>
         </main>
     )
 }
