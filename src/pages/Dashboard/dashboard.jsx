b6871400b6970221f93fca848f807271f190a12d
Ik3nna <iamiykendukwu@gmail.com>
2023-01-05 18:55:44 +0100

    Feat: Fetching of data from API and implementation of the loading component Style: Styling of the loading component

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./dashboard.module.scss";
 import Navbar from "../../components/Navbar/navbar";
 
+const url = "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users";
+
 function Dashboard () {
+    const [loading, setLoading] = useState(false);
+    const [dashboardData, setDashboardData] = useState([]);
+
+    const fetchData = ()=> {
+        setLoading(true);
+        
+        fetch(url)
+        .then(response => {
+          if(response.ok) {
+            return response;
+          }
+          else {
+            let error = new Error("Error " + response.status + ": " + response.statusText);
+            error.response = response;
+            throw error;
+          }
+        },
+        error => {
+          let errmess = new Error(error.message);
+          throw errmess;
+        })
+        .then(response => response.json())
+        .then(data => {
+          setDashboardData(data)
+          setLoading(false)
+        })
+        .catch(error => {
+          setLoading(false)
+          console.log(error.message)
+        })
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    if (loading) {
+        return(
+            <main className={styles.loading}>
+                <h3>Loading...</h3>
+            </main>
+        );
+    }
+
     return(
         <>
             <Navbar />
@@ -47,6 +93,7 @@ function Dashboard () {
                 </section>
 
                 
+
             </main>
         </>
     );
