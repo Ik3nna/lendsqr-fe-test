7d49c2598287618319e2388ac4f2ab299ca710db
Ik3nna <iamiykendukwu@gmail.com>
2023-01-10 09:26:53 +0100

    Feat: Further setup of the pagination component and table

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import styles from "./dashboard.module.scss";
 import Navbar from "../../components/Navbar/navbar";
+import Pagination from "../../components/Pagination/pagination";
+import Table from 'react-bootstrap/Table';
 
 const url = "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users";
+
+let PageSize = 10;
 
 function Dashboard () {
     const [loading, setLoading] = useState(false);
@@ -39,18 +43,27 @@ function Dashboard () {
 
     useEffect(()=>{
         fetchData();
-    },[])
+    },[]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return dashboardData.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, dashboardData]);
+
 
     if (loading) {
         return(
             <main className={styles.loading}>
-                <h3>Loading...</h3>
+                <div>Loading...</div>
             </main>
-        );
+        )
     }
 
     return(
-        <>
+        <section className={styles.sf}>
             <Navbar />
 
             <main className={styles.content}>
@@ -92,10 +105,53 @@ function Dashboard () {
                     </article>
                 </section>
 
-                
+                <Table size="md" responsive="xl">
+                    <thead>
+                        <tr>
+                            <th>organization</th>
+                            <th>username</th>
+                            <th>email</th>
+                            <th>phone number</th>
+                            <th>date joined</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {currentTableData.map((item, index)=>{
+                            const dateJoined = (new Date(item.createdAt.substring(0, 10).split("-")).toDateString());
+                            const status = (new Date(item.lastActiveDate.substring(0, 10).split("-")).toDateString())
+
+                            return(
+                                <tr key={index}>
+                                    <td>{item.orgName}</td>
+                                    <td>{item.userName}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.phoneNumber}</td>
+                                    <td>{dateJoined}</td>
+                                    <td>{status}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+
+                <div className={styles.pageContainer}>
+                    <div className={styles.show}>
+                        Showing&nbsp;<span>{currentPage * PageSize}</span>&nbsp;out of {dashboardData.length}
+                    </div>
+
+                    <Pagination
+                        className={styles.page}
+                        currentPage={currentPage}
+                        totalCount={dashboardData.length}
+                        pageSize={PageSize}
+                        onPageChange={page => setCurrentPage(page)}
+                    />
+                </div>
 
             </main>
-        </>
+        </section>
     );
 }
 
