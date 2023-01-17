b72845199d714df6a03777d62dcde79c234aeb39
Ik3nna <iamiykendukwu@gmail.com>
2023-01-17 19:38:16 +0100

    Feat & Style: Finished the user details page and styled it acordingly

--- src/pages/User-details/user.jsx
+++ src/pages/User-details/user.jsx
@@ -38,7 +38,7 @@ function User () {
                     </div>
 
                     <article className={styles.header}>
-                        <img src="/assets/avatar.svg" alt="avatar" />
+                        <img src={userarr?.profile?.avatar} alt="avatar" />
 
                         <div>
                             {userarr?.profile?.firstName}&nbsp;{userarr?.profile?.lastName}
@@ -62,6 +62,144 @@ function User () {
                             <div>Savings</div>
                             <div>App and System</div>
                         </div>
+                    </article>
+
+                    <article className={styles.mdeets}>
+                        <section>
+                            <h5>Personal Information</h5>
+
+                            <article>
+                                <div>
+                                    <p>full name</p>
+                                    <p>{userarr?.profile?.firstName}&nbsp;{userarr?.profile?.lastName}</p>
+                                </div>
+
+                                <div>
+                                    <p>phone number</p>
+                                    <p>{userarr?.profile?.phoneNumber}</p>
+                                </div>
+
+                                <div>
+                                    <p>email address</p>
+                                    <p>{userarr?.email}</p>
+                                </div>
+
+                                <div>
+                                    <p>bvn</p>
+                                    <p>{userarr?.profile?.bvn}</p>
+                                </div>
+
+                                <div>
+                                    <p>gender</p>
+                                    <p>{userarr?.profile?.gender}</p>
+                                </div>
+
+                                <div>
+                                    <p>marital status</p>
+                                    <p>Single</p>
+                                </div>
+
+                                <div>
+                                    <p>children</p>
+                                    <p>None</p>
+                                </div>
+
+                                <div>
+                                    <p>type of residence</p>
+                                    <p>{userarr?.profile?.address}</p>
+                                </div>
+                            </article>
+
+                        </section><hr />
+
+                        <section>
+                            <h5>Education and Employment</h5>
+
+                            <article className={styles.ee}>
+                                <div>
+                                    <p>level of education</p>
+                                    <p>{userarr?.education?.level}</p>
+                                </div>
+
+                                <div>
+                                    <p>employment status</p>
+                                    <p>{userarr?.education?.employmentStatus}</p>
+                                </div>
+
+                                <div>
+                                    <p>sector of employment</p>
+                                    <p>{userarr?.education?.sector}</p>
+                                </div>
+
+                                <div>
+                                    <p>duration of employment</p>
+                                    <p>{userarr?.education?.duration}</p>
+                                </div>
+
+                                <div>
+                                    <p>office email</p>
+                                    <p>{userarr?.education?.officeEmail}</p>
+                                </div>
+
+                                <div>
+                                    <p>monthly income</p>
+                                    <p>&#8358;{userarr?.education?.monthlyIncome[1]}&nbsp;-&nbsp;&#8358;{userarr?.education?.monthlyIncome[0]}</p>
+                                </div>
+
+                                <div>
+                                    <p>loan repaymet</p>
+                                    <p>None</p>
+                                </div>
+                            </article>
+    
+                        </section><hr />
+
+                        <section>
+                            <h5>Socials</h5>
+
+                            <article>
+                                <div>
+                                    <p>twitter</p>
+                                    <p>{userarr?.socials?.twitter}</p>
+                                </div>
+
+                                <div>
+                                    <p>facebook</p>
+                                    <p>{userarr?.socials?.facebook}</p>
+                                </div>
+
+                                <div>
+                                    <p>instagram</p>
+                                    <p>{userarr?.socials?.instagram}</p>
+                                </div>
+                            </article>
+                        </section><hr />
+
+                        <section>
+                            <h5>Guarantor</h5>
+
+                            <article>
+                                <div>
+                                    <p>full name</p>
+                                    <p>{userarr?.guarantor?.firstName}&nbsp;{userarr?.guarantor?.lastName}</p>
+                                </div>
+
+                                <div>
+                                    <p>phone number</p>
+                                    <p>{userarr?.guarantor?.phoneNumber}</p>
+                                </div>
+
+                                <div>
+                                    <p>gender</p>
+                                    <p>{userarr?.guarantor?.gender}</p>
+                                </div>
+
+                                <div>
+                                    <p>address</p>
+                                    <p>{userarr?.guarantor?.address}</p>
+                                </div>
+                            </article>
+                        </section>
                     </article>
                 </article>
             </section>
